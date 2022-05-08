5f492bc78e89a25978ebd4143563cb98596f3254
Arik <BBArik@protonmail.com>
2022-05-07 23:26:36 -0400

    Add documentation to methods

--- rss_parser/_parser.py
+++ rss_parser/_parser.py
@@ -1,5 +1,5 @@
 import re
-from typing import Optional
+from typing import Any, List, Optional
 
 from bs4 import BeautifulSoup
 
@@ -7,6 +7,10 @@ from .models import RSSFeed
 
 
 class Parser:
+    """
+    Parser for rss files
+    """
+
     def __init__(self, xml: str, limit=None):
         self.xml = xml
         self.limit = limit
@@ -16,15 +20,29 @@ class Parser:
 
     @staticmethod
     def get_soup(xml: str, parser: str = "xml") -> BeautifulSoup:
+        """Get the BeautifulSoup object with a specified parser.
+
+        :param xml: The xml content
+        :param parser: The parser type. Default is xml
+        :return: The BeautifulSoup object
+        """
         return BeautifulSoup(xml, parser)
 
     @staticmethod
     def check_none(
-        item: object,
-        default: str,
-        item_dict: Optional[str] = None,
-        default_dict: Optional[str] = None,
-    ):
+            item: object,
+            default: str,
+            item_dict: Optional[str] = None,
+            default_dict: Optional[str] = None,
+    ) -> Any:
+        """Check if the item_dict in item is None, else returns default_dict of default.
+
+        :param item: The first object.
+        :param default: The default object.
+        :param item_dict: The item dictionary.
+        :param default_dict: The default dictionary.
+        :return: The (not None) final object.
+        """
         if item:
             return item[item_dict]
         else:
@@ -35,9 +53,27 @@ class Parser:
 
     @staticmethod
     def get_text(item: object, attribute: str) -> str:
+        """Returns the text information about an attribute of an object.
+        If it is not present, it will return an empty string.
+
+        :param item: The object with the attribute
+        :param attribute: The attribute which has a 'text' attribute
+        :return: The string of the text of the specified attribute
+        """
         return getattr(getattr(item, attribute, ""), "text", "")
 
-    def parse(self, entries: []) -> RSSFeed:
+    def parse(self, entries: Optional[List[str]] = List) -> RSSFeed:
+        """
+        Parse the rss and each item of the feed.
+
+        Missing attributes will be replaced by an empty string. The
+        information of the optional entries are stored in a dictionary
+        under the attribute "other" of each item.
+
+        :param entries: An optional list of additional rss tags that can be recovered
+        from each item
+        :return: The RSSFeed which describe the rss information
+        """
         main_soup = self.get_soup(self.xml)
 
         self.raw_data = {
@@ -56,8 +92,10 @@ class Parser:
         for item in items:
             # Using html.parser instead of lxml because lxml can't parse <link>
             description_soup = self.get_soup(
-                self.get_text(item, "description"), "html.parser"
-            )
+                self.get_text(
+                    item,
+                    "description"
+                ), "html.parser")
 
             item_dict = {
                 "title": self.get_text(item, "title"),
