420a95773a2fdd7b2802b4a5394793567f88536c
David Kasakaitis <davidkasakaitis@proton.me>
2024-02-13 18:39:18 +0000

    Changed entrypoint to accomodate Atom and new dir structure

--- rss_parser/_parser.py
+++ rss_parser/_parser.py
@@ -1,27 +1,27 @@
-from typing import ClassVar, Optional, Type
+from typing import Optional, Type
 
 from xmltodict import parse
 
 from rss_parser.models import XMLBaseModel
+from rss_parser.models.atom import Atom
 from rss_parser.models.rss import RSS
 
 # >>> FUTURE
 # TODO: May be support generator based approach for big rss feeds
 # TODO: Add cli to parse to json
 # TODO: Possibly bundle as deb/rpm/exe
-# TODO: Atom support
-# TODO: Older RSS versions?
+# TODO: Older Atom versions
+# TODO: Older RSS versions
 
 
 class Parser:
     """Parser for rss files."""
 
-    schema: ClassVar[Type[XMLBaseModel]] = RSS
-
     @staticmethod
-    def _check_atom(root: dict):
+    def check_schema(root: dict) -> tuple[dict, type[XMLBaseModel]]:
         if "feed" in root:
-            raise NotImplementedError("ATOM feed is not currently supported")
+            return root, Atom
+        return root["rss"], RSS
 
     @staticmethod
     def to_xml(data: str, *args, **kwargs):
@@ -36,8 +36,6 @@ class Parser:
         :return: "schema" object
         """
         root = cls.to_xml(data)
-        cls._check_atom(root)
+        root, schema = cls.check_schema(root)
 
-        schema = schema or cls.schema
-
-        return schema.parse_obj(root["rss"])
+        return schema.parse_obj(root)
