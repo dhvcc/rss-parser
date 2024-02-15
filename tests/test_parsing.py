8e3c4fbfe3fe39ea1815e310f2b41b6fbedecc10
David Kasakaitis <davidkasakaitis@proton.me>
2024-02-15 15:24:29 +0000

    Testing changes to allow for new parser structure

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -2,17 +2,17 @@ from json import dumps
 
 import pytest
 
-from rss_parser import Parser
+from rss_parser import AtomParser, RSSParser
 
 
 @pytest.mark.parametrize(
     "sample_and_result",
-    [["rss_2"], ["rss_2_no_category_attr"], ["apology_line"], ["rss_2_with_1_item"], ["atom"]],
+    [["rss_2"], ["rss_2_no_category_attr"], ["apology_line"], ["rss_2_with_1_item"]],
     indirect=True,
 )
-def test_parses_all_samples(sample_and_result):
+def test_parses_all_rss_samples(sample_and_result):
     sample, result = sample_and_result
-    rss = Parser.parse(sample)
+    rss = RSSParser.parse(sample)
 
     assert rss
 
@@ -28,7 +28,7 @@ def test_parses_all_samples(sample_and_result):
 def test_json_plain_ignores_attributes(sample_and_result):
     # Expect basic RSSv2 to be parsed
     sample, result = sample_and_result
-    rss = Parser.parse(sample)
+    rss = RSSParser.parse(sample)
 
     assert rss
 
@@ -38,7 +38,18 @@ def test_json_plain_ignores_attributes(sample_and_result):
     assert left == right
 
 
-# def test_fails_atom_feed(atom_feed):
-#     # Expect ATOM feed to fail since it's not supported
-#     with pytest.raises(NotImplementedError):
-#         Parser.parse(atom_feed)
+@pytest.mark.parametrize(
+    "sample_and_result",
+    [["atom"]],
+    indirect=True,
+)
+def test_parses_all_atom_samples(sample_and_result):
+    sample, result = sample_and_result
+    atom = AtomParser.parse(sample)
+
+    assert atom
+
+    left = atom.json(indent=2, sort_keys=True)
+    right = dumps(result, indent=2, sort_keys=True, default=str)
+
+    assert left == right
