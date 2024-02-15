ffee8e81a74adc1a4a698dac21b6515e0c1a0416
David Kasakaitis <davidkasakaitis@proton.me>
2024-02-15 15:25:41 +0000

    New parser object structure implementation

--- rss_parser/_parser.py
+++ rss_parser/_parser.py
@@ -1,7 +1,8 @@
-from typing import Optional, Type
+from typing import ClassVar, Optional, Type
 
 from xmltodict import parse
 
+from rss_parser.custom_decorators import abstract_class_attributes
 from rss_parser.models import XMLBaseModel
 from rss_parser.models.atom import Atom
 from rss_parser.models.rss import RSS
@@ -14,31 +15,59 @@ from rss_parser.models.rss import RSS
 # TODO: Older RSS versions
 
 
-class Parser:
+@abstract_class_attributes("schema")
+class BaseParser:
     """Parser for rss/atom files."""
 
-    @staticmethod
-    def check_schema(root: dict) -> tuple[dict, type[XMLBaseModel]]:
-        if "feed" in root:
-            return root, Atom
-        return root["rss"], RSS
+    schema: ClassVar[Type[XMLBaseModel]]
+    root_key: Optional[str] = None
 
     @staticmethod
     def to_xml(data: str, *args, **kwargs):
         return parse(str(data), *args, **kwargs)
 
     @classmethod
-    def parse(cls, data: str, *, schema: Optional[Type[XMLBaseModel]] = None, root_key: str = "") -> XMLBaseModel:
+    def parse(
+        cls,
+        data: str,
+        *,
+        schema: Optional[Type[XMLBaseModel]] = None,
+        root_key: Optional[str] = None,
+    ) -> XMLBaseModel:
         """
-        Parse XML data into schema (default: RSS 2.0 or Atom).
-
+        Parse XML data into schema.
         :param data: string of XML data that needs to be parsed
         :return: "schema" object
         """
         root = cls.to_xml(data)
-        if not isinstance(schema, XMLBaseModel):
-            root, schema = cls.check_schema(root)
-        else:
+
+        schema = schema if schema else cls.schema
+
+        root_key = root_key if root_key else cls.root_key
+
+        if root_key:
             root = root.get(root_key, root)
 
         return schema.parse_obj(root)
+
+
+class AtomParser(BaseParser):
+    schema = Atom
+
+
+class RSSParser(BaseParser):
+    root_key = "rss"
+    schema = RSS
+
+
+class Parser(RSSParser):
+    @classmethod
+    def parse(cls, data: str, *, schema: Optional[Type[XMLBaseModel]] = None) -> XMLBaseModel:
+        import warnings
+
+        warnings.warn(
+            "Class Parser was renamed to RSSParser " "and will be removed in the next major update",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return RSSParser.parse(data, schema=schema)
