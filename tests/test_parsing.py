07d416f57ba3e998a0b5b4ed73fb8d59153d74b9
dhvcc <alexey.artishevskiy@gmail.com>
2024-02-22 21:03:34 +0300

    Add more atom tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -7,7 +7,12 @@ from rss_parser import AtomParser, RSSParser
 
 @pytest.mark.parametrize(
     "sample_and_result",
-    [["rss_2"], ["rss_2_no_category_attr"], ["apology_line"], ["rss_2_with_1_item"]],
+    [
+        ["rss_2"],
+        ["rss_2_no_category_attr"],
+        ["apology_line"],
+        ["rss_2_with_1_item"],
+    ],
     indirect=True,
 )
 def test_parses_all_rss_samples(sample_and_result):
@@ -40,7 +45,10 @@ def test_json_plain_ignores_attributes(sample_and_result):
 
 @pytest.mark.parametrize(
     "sample_and_result",
-    [["atom"]],
+    [
+        ["atom"],
+        ["generic_atom_feed"],
+    ],
     indirect=True,
 )
 def test_parses_all_atom_samples(sample_and_result):
@@ -50,6 +58,27 @@ def test_parses_all_atom_samples(sample_and_result):
     assert atom
 
     left = atom.json(indent=2, sort_keys=True)
+    right = dumps(result, indent=2, sort_keys=True, default=str)
+
+    assert left == right
+
+
+@pytest.mark.parametrize(
+    "sample_and_result",
+    [
+        ["atom", True],
+        ["generic_atom_feed", True],
+    ],
+    indirect=True,
+)
+def test_json_plain_ignores_attributes_atom(sample_and_result):
+    # Expect basic RSSv2 to be parsed
+    sample, result = sample_and_result
+    rss = AtomParser.parse(sample)
+
+    assert rss
+
+    left = rss.json_plain(indent=2, sort_keys=True)
     right = dumps(result, indent=2, sort_keys=True, default=str)
 
     assert left == right
