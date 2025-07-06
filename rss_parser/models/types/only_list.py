0c607a74b57d6ae507931164b21638b19d2b48b5
dhvcc <alexey.artishevskiy@gmail.com>
2025-07-06 21:03:21 +0200

    v2.1.1 Fix improper generic setup for mypy #51

--- rss_parser/models/types/only_list.py
+++ rss_parser/models/types/only_list.py
@@ -1,11 +1,13 @@
-from typing import Union
+from typing import Generic, TypeVar, Union
 
 from rss_parser.pydantic_proxy import import_v1_pydantic
 
 pydantic_validators = import_v1_pydantic(".validators")
 
+T = TypeVar("T")
 
-class OnlyList(list):
+
+class OnlyList(list, Generic[T]):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
@@ -14,8 +16,8 @@ class OnlyList(list):
     @classmethod
     def validate(cls, v: Union[dict, list]):
         if isinstance(v, list):
-            return v
-        return [v]
+            return cls(v)
+        return cls([v])
 
     def __repr__(self):
         return f"OnlyList({super().__repr__()})"
